387d4fd7bd1cd4a3ad5ccbc3bfc3ab4583a6f326
Philipp Hofmann <ph0hofmann@gmail.com>
2020-01-21 15:59:16 +0100

    Remove comment RxJava SchedulerProvider on Repository

--- common/business/src/main/kotlin/com/muffls/common/business/Repository.kt
+++ common/business/src/main/kotlin/com/muffls/common/business/Repository.kt
@@ -2,10 +2,6 @@ package com.muffls.common.business
 
 import kotlinx.coroutines.flow.Flow
 
-/**
- * It is guaranteed that all methods don't run on the [com.muffls.libs.rxjava2.SchedulerProvider.ui]
- * thread.
- */
 interface Repository<T> {
 
     val all: Flow<List<T>>
